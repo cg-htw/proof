ae4ff0d838fcea26c804c58d8c03f0b2cd568ae7
jaanis7 <janis.schanbacher@posteo.de>
2019-07-02 13:50:50 +0200

    Cleanup

--- source/CGTutorial/source/Behaviour.hpp
+++
@@ -1,7 +0,0 @@
-class Behaviour {
-public:
-    Behaviour();   // Constructor.
-    virtual void update() = 0;
-    virtual void addModel( Model model );
-    virtual void removeModel( Model model );
-};
--- source/CGTutorial/source/main.cpp
+++ source/CGTutorial/source/main.cpp
@@ -92,10 +92,6 @@ GLuint transparentTexture;
 
 GLuint skyboxVAO, skyboxVBO;
 
-Camera camera( glm::vec3(0.0f, 0.0f, 3.0f) );
-
-
-
 // Function prototypes
 void error_callback( int error, const char* description );
 void key_callback( GLFWwindow* window, int key, int scancode, int action, int mods );
